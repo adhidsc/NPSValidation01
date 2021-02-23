9d94e56dc1d5287b174313d9e5b5454d17ae2282
adhidsc <adhidscientist@gmail.com>
2021-02-23 11:43:51 +0530

    Comment added to verify the last committed message
    Comment added to verify the last committed message

--- src/AzureSeleniumTest.java
+++ src/AzureSeleniumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
  
 public class AzureSeleniumTest {   
 /**
-* @param args
+* Comment added to verify the last committed message
 */
        public static void main(String[] args) {
               
